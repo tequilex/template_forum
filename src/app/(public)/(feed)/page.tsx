f2a47d7d6b8d84d08c79f4b1d09b1745b1e1ba12
tequilex <dron2010xxx@mail.ru>
2026-07-02 21:38:16 +0300

    test: temp console.log on home page to verify rebuild pipeline

--- src/app/(public)/(feed)/page.tsx
+++ src/app/(public)/(feed)/page.tsx
@@ -19,6 +19,7 @@ interface PageProps {
 }
 
 export default async function HomePage({ searchParams }: PageProps) {
+  console.log("pipiska");
   const sp = await searchParams;
   const page = Number(sp.page ?? "1") || 1;
 
